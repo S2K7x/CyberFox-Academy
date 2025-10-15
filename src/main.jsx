2beb0157a2eb6be483d90b7ecf109eb3194728c4
S2K7x <166259891+S2K7x@users.noreply.github.com>
2025-10-16 00:31:09 +0300

    Fix import path for CyberfoxLanding component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CyberfoxLanding from './CyberfoxLanding';
 import './index.css';
-import CyberfoxLanding from './components/CyberfoxLanding';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
