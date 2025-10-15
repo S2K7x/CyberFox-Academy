c667714a68576f0fd150c633b029671f43580312
S2K7x <166259891+S2K7x@users.noreply.github.com>
2025-10-16 00:41:07 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CyberfoxLanding from './components/CyberfoxLanding.jsx';
+import CyberfoxLanding from '/components/CyberfoxLanding.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
