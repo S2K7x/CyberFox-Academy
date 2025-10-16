f1becf7cd49d6e54fc2873bd4428bba80b051172
S2K7x <166259891+S2K7x@users.noreply.github.com>
2025-10-16 09:27:31 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,css}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
